18751eaa1a14f8c0e645a8b1161c0d82c669129d
Souvik Biswas <sbis1999@gmail.com>
2022-01-23 13:14:21 +0530

    Update color

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.deepOrange,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
